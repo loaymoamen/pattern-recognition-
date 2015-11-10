cd40f092837deee1d61a8fda5df8ab404350d6ca
Loay Moamen <loaymoamen@gmail.com>
2015-11-11 00:58:08 +0200

    Switching input data to matching data types

--- get_features.py
+++ get_features.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image
 
-img = Image.open('8-2.jpg').convert('1')
+img = Image.open('ass1/data/8-2.jpg').convert('L')
+img = img.resize((8,8), Image.ANTIALIAS)
 arr = np.array(img)
 flat_arr = arr.ravel()
+flat_arr = 15 - (flat_arr/16)
